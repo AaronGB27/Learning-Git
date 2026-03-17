b0ff74fccdad0da24210ba092cc9a5c95099c736
AaronGB27 <153691679+AaronGB27@users.noreply.github.com>
2026-03-16 23:48:31 -0500

    Rename variable 'creating_user' to 'creating_username'

--- loginhaha.py
+++ loginhaha.py
@@ -1,4 +1,4 @@
-creating_user = input("Enter a name: ").capitalize()
+creating_username = input("Enter a name: ").capitalize()
 creating_pin = int(input("Enter a pin: "))
 print("Enter your own credentials your username and pin to get the access")
 user = input("Username: ").capitalize
